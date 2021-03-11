d114c8d81566c86e8142ef10c465e085c37fe5c6
samarbouzezi <samar.bouzezi@esprit.tn>
2021-03-11 11:34:40 +0100

    samar atelier crud

---
+++ atelier_crud/mainwindow.cpp
@@ -0,0 +1,58 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "etudiant.h"
+#include <QMessageBox>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+ui->setupUi(this);
+ui->tabetudiant->setModel(tmpetudiant.afficher());
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pb_ajouter_clicked()
+{
+    int id = ui->lineEdit_id->text().toInt();
+    QString nom= ui->lineEdit_nom->text();
+    QString prenom= ui->lineEdit_prenom->text();
+  Etudiant e(id,nom,prenom);
+  bool test=e.ajouter();
+  if(test)
+{ui->tabetudiant->setModel(tmpetudiant.afficher());//refresh
+QMessageBox::information(nullptr, QObject::tr("Ajouter un étudiant"),
+                  QObject::tr("Etudiant ajouté.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+  else
+      QMessageBox::critical(nullptr, QObject::tr("Ajouter un étudiant"),
+                  QObject::tr("Erreur !.\n"
+                              "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+}
+
+void MainWindow::on_pb_supprimer_clicked()
+{
+int id = ui->lineEdit_id->text().toInt();
+bool test=tmpetudiant.supprimer(id);
+if(test)
+{ ui->tabetudiant->setModel(tmpetudiant.afficher());//refresh
+    QMessageBox::information(nullptr, QObject::tr("Supprimer un étudiant"),
+                QObject::tr("Etudiant supprimé.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+else
+    QMessageBox::critical(nullptr, QObject::tr("Supprimer un étudiant"),
+                QObject::tr("Erreur !.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+}
