3fe5dfa233f9fa5655ad07e9a572ff1a795017ee
MedAzizBenzarti <mohamedaziz.benzarti@esprit.tn>
2021-03-30 11:42:48 +0100

    aziz_metier:mailing(+)_image_crud

---
+++ aziz_crud_metier/sport.cpp
@@ -0,0 +1,180 @@
+#include "sport.h"
+#include <QSqlQuery>
+#include <QDebug>
+#include <QObject>
+#include <QPrinter>
+#include <QFileDialog>
+#include <QTextDocument>
+#include <QTextDocument>
+#include <QMessageBox>
+#include <QPainter>
+#include "sport_entraineur.h"
+#include "ui_sport_entraineur.h"
+#include<QTextDocument>
+
+sport::sport()
+{
+  id=0;
+  nom="";
+  dat="";
+  nb=0;
+}
+
+sport::sport(int id,QString nom, QString dat ,int nb)
+{
+this->id=id;
+    this->nom=nom;
+    this->dat=dat;
+    this->nb=nb;
+}
+
+int sport::getID()
+{
+    return id;
+}
+QString sport::getNom()
+{return nom;}
+QString sport::getdat()
+{return dat;}
+int sport::getnb()
+{return nb;}
+
+
+void sport::setId(int id)
+{this->id=id;}
+void sport::setNom(QString nom)
+{this->nom=nom;}
+void sport::setdat(QString dat)
+{this->dat=dat;}
+void sport::setnb(int nb)
+{this->nb=nb;}
+
+
+
+bool sport::ajouter(){
+    QSqlQuery query;
+    QString id_string= QString::number(id);
+
+       QString nb_string= QString::number(nb);
+          query.prepare("INSERT INTO sport ( id, nom, dat , nb) "
+                        "VALUES (:id, :nom, :dat, :nb )");
+          query.bindValue(":id", id_string);
+          query.bindValue(":nom", nom);
+          query.bindValue(":dat",dat);
+          query.bindValue(":nb", nb_string);
+
+
+         return query.exec();
+}
+
+void sport::afficher_sport(Ui::sport_entraineur *ui)
+{
+    QSqlQuery q;
+    QSqlQueryModel *modal=new QSqlQueryModel();
+    q.prepare("select * from sport");
+    q.exec();
+    modal->setQuery(q);
+    ui->tableView->setModel(modal);
+}
+
+
+
+bool sport::Supprimer_sport(Ui::sport_entraineur *ui)
+{
+    QSqlQuery q;
+    QString id = ui->tableView->model()->data(ui->tableView->model()->index(ui->tableView->selectionModel()->currentIndex().row(),0)).toString();
+    q.prepare("DELETE FROM sport WHERE id ='"+id+"'");
+    q.exec();
+}
+
+bool sport::modifier_sport(Ui::sport_entraineur *ui)
+{
+
+        QSqlQuery q;
+
+        QString id=ui->tableView->model()->data(ui->tableView->model()->index(ui->tableView->selectionModel()->currentIndex().row(),0)).toString();
+
+        q.prepare("UPDATE sport set nom:=nom_modif,dat:=dat_modif, nb:=nb_modif where id='"+id+"'");
+
+        q.bindValue(":nom_modif",ui->nom_modif->text());
+        q.bindValue(":dat_modif",ui->date_modif->text());
+
+        q.bindValue(":nb_modif",ui->nb_modif->text());
+
+        return q.exec();
+
+    }
+
+void sport::Recherche_sport(Ui::sport_entraineur *ui)
+{
+    QSqlQuery q;
+    QSqlQueryModel *modal=new QSqlQueryModel();
+    QString rech =ui->recherche->text();
+    q.prepare("select * from sport where ( dat LIKE '%"+rech+"%' or nom LIKE '%"+rech+"%' or nb LIKE'%"+rech+"%' )");
+
+    if ( q.exec() )
+    { modal->setQuery(q);
+      ui->tableView->setModel(modal);}
+
+}
+
+void sport::Tri_NOM(Ui::sport_entraineur *ui)
+{
+    QSqlQuery q;
+    QSqlQueryModel *model=new QSqlQueryModel();
+    q.prepare("select * from sport order by nom");
+    q.exec();
+    model->setQuery(q);
+    ui->tableView->setModel(model);
+}
+void sport::Tri_NB(Ui::sport_entraineur *ui)
+{
+    QSqlQuery q;
+    QSqlQueryModel *model=new QSqlQueryModel();
+    q.prepare("select * from sport order by nb");
+    q.exec();
+    model->setQuery(q);
+    ui->tableView->setModel(model);
+}
+void sport::Tri_DAT(Ui::sport_entraineur *ui)
+{
+    QSqlQuery q;
+    QSqlQueryModel *model=new QSqlQueryModel();
+    q.prepare("select * from sport order by dat");
+    q.exec();
+    model->setQuery(q);
+    ui->tableView->setModel(model);
+}
+
+
+void sport::image()
+{ QImage image(450, 200, QImage::Format_ARGB32_Premultiplied);
+    QPainter painter(&image);
+    painter.fillRect(image.rect(), Qt::yellow);
+    QSqlQuery q;
+            q.prepare("SELECT * FROM sport ");
+            q.exec();
+            while(q.next())
+            {
+                QString image1,image2;
+                image1=q.value(1).toString();
+                image2=q.value(2).toString();
+    painter.drawText(image.rect(), Qt::AlignCenter | Qt::AlignLeft,image1);
+    image.save("output.png");
+            }
+    QMessageBox::information(nullptr, QObject::tr("OK"),
+                QObject::tr("ajout de l'image succesful.\n"
+                            "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+
+
+int sport::getValide()
+{
+    return valide;
+}
+
+void sport::setValide()
+{
+valide=1;
+}
---
+++ aziz_crud_metier/sport_entraineur.cpp
@@ -0,0 +1,370 @@
+#include "sport_entraineur.h"
+#include "ui_sport_entraineur.h"
+#include"entraineur.h"
+#include"sport.h"
+#include<QDebug>
+#include <QMessageBox>
+#include <QMediaPlayer>
+#include <QObject>
+#include <QPrinter>
+#include <QFileDialog>
+#include <QTextDocument>
+#include <QPropertyAnimation>
+#include <QPainter>
+#include <QSound>
+#include "smtp_entraineur.h"
+#include "email_entraineur.h"
+
+sport_entraineur::sport_entraineur(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::sport_entraineur)
+{
+    ui->setupUi(this);
+    son=new QSound(":/sons/bruit-clic.wav");
+             supp=new QSound(":/sons/supprimer.wav");
+              ajout=new QSound(":/sons/ajouter.wav");
+    entraineur e ;
+
+    e.afficher(ui);
+    sport s;
+    s.afficher_sport(ui);
+}
+
+sport_entraineur::~sport_entraineur()
+{
+    delete ui;
+}
+
+void  sport_entraineur::on_supprimer_clicked()
+{
+    supp->play();
+    sport e;
+    e.Supprimer_sport(ui);
+    e.afficher_sport(ui);
+}
+
+void  sport_entraineur::on_comboBox_currentIndexChanged(int index)
+{
+    sport e;
+    if (index==0)
+    {e.Tri_NOM(ui);
+
+    }
+
+    else if(index==1)
+    {  e.Tri_DAT(ui);
+
+    }
+    else
+    {e.Tri_NB(ui);
+    }
+}
+
+
+
+
+void  sport_entraineur::on_search_clicked()
+{
+    sport e;
+    son->play();
+  e.Recherche_sport(ui);
+
+}
+
+void sport_entraineur::on_tableView_doubleClicked(const QModelIndex &index)
+{
+    ui->nom_modif->setText(ui->tableView->model()->data(ui->tableView->model()->index(ui->tableView->selectionModel()->currentIndex().row(),1)).toString());
+    ui->nb_modif->setText(ui->tableView->model()->data(ui->tableView->model()->index(ui->tableView->selectionModel()->currentIndex().row(),2)).toString());
+    //ui->tel_modif->setText(ui->tableView->model()->data(ui->tableView->model()->index(ui->tableView->selectionModel()->currentIndex().row(),4)).toString());
+    ui->date_modif->setText(ui->tableView->model()->data(ui->tableView->model()->index(ui->tableView->selectionModel()->currentIndex().row(),5)).toString());
+    ui->id_modif->setText(ui->tableView->model()->data(ui->tableView->model()->index(ui->tableView->selectionModel()->currentIndex().row(),0)).toString());
+    ui->stackedWidget->setCurrentIndex(1);
+
+}
+
+void sport_entraineur::on_modifier_clicked()
+{
+    sport e;
+   ajout->play();
+    if(e.modifier_sport(ui))
+    {
+        QMessageBox ::information(this,"","sport modifié")  ;
+        ui->stackedWidget->setCurrentIndex(0);
+        e.afficher_sport(ui);
+    }
+    else
+    { QMessageBox ::critical(this,"","erreur de modification") ;}
+
+}
+
+
+void sport_entraineur::on_Valider_clicked()
+
+{
+    ajout->play();
+    QRegExp regex1 ("[a-z]$");
+    QRegExp regex2 ("^[a-z]");
+
+    QString msg_noms ="nom invalide";
+    QString msg_nb ="type invalide";
+    QString msg_vide="";
+
+    int id=ui->id->text().toInt();
+    QString NOM=ui->nom->text();
+    QString dat=ui->date->text();
+
+     int nb=ui->nb->text().toInt();
+
+    sport F(id,NOM,dat,nb);
+
+    if ((!NOM.contains(regex1))||(!NOM.contains(regex2)))
+    {
+        ui->nom->setStyleSheet("QLineEdit { color: red;}");
+        ui->text_noms->setText(msg_noms);
+        ui->text_noms->setStyleSheet("QLabel { background-color : transparent; color : red; }");
+        F.setValide();
+    }
+    else { ui->nom->setStyleSheet("QLineEdit { color: black;}");
+        ui->text_noms->setText(msg_vide);
+
+    }
+    if ((nb<2)||(nb>50))
+    {
+        ui->nb->setStyleSheet("QLineEdit { color: red;}");
+        ui->text_nb->setText(msg_nb);
+        ui->text_nb->setStyleSheet("QLabel { background-color : transparent; color : red; }");
+        F.setValide();
+    }
+    else { ui->nb->setStyleSheet("QLineEdit { color: black;}");
+        ui->text_nb->setText(msg_vide);
+
+    }
+
+
+    if (F.getValide()==0)
+    {
+    if(F.ajouter()){
+        ui->nom->setStyleSheet("QLineEdit { color: green;}");
+        ui->nb->setStyleSheet("QLineEdit { color: green;}");
+        QMessageBox::information(nullptr, QObject::tr("OK"),
+                    QObject::tr("ajout de sport succesful.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+          //ui->tableView->setModel(F.afficher());
+    }
+    else{
+
+        QMessageBox::critical(nullptr, QObject::tr("NOT OK"),
+                    QObject::tr("ajout failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+    }
+
+}
+}
+
+
+void sport_entraineur::on_Annuler_clicked()
+{
+    son->play();
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void sport_entraineur::on_sport_clicked()
+{   son->play();
+    sport e;
+    e.afficher_sport(ui);
+    ui->stackedWidget_ajout->setCurrentIndex(1);
+}
+
+void sport_entraineur::on_annuler_clicked()
+{ sport e;
+son->play();
+    ui->stackedWidget_ajout->setCurrentIndex(0);
+    e.ajouter();
+
+}
+
+
+void sport_entraineur::on_supprimer_2_clicked()
+{
+    supp->play();
+    entraineur s;
+    s.Supprimer(ui);
+    s.afficher(ui);
+}
+
+
+
+
+void sport_entraineur::on_comboBox_2_currentIndexChanged(int index)
+{
+
+    entraineur s;
+    if (index==0)
+    {s.Tri_NOM(ui);
+
+    }
+
+    else if(index==1)
+    {  s.Tri_prenom(ui);
+
+    }
+    else
+    {s.Tri_DATE(ui);
+    }
+}
+
+void sport_entraineur::on_search_2_clicked()
+{
+    ajout->play();
+    entraineur s;
+    s.Recherche(ui);
+}
+
+void sport_entraineur::on_tableView_2_doubleClicked(const QModelIndex &index)
+{
+    ui->nom_modif_en->setText(ui->tableView_4->model()->data(ui->tableView_4->model()->index(ui->tableView_4->selectionModel()->currentIndex().row(),1)).toString());
+    ui->prenom_modif_en->setText(ui->tableView_4->model()->data(ui->tableView_4->model()->index(ui->tableView_4->selectionModel()->currentIndex().row(),2)).toString());
+    ui->dat_modif_en->setText(ui->tableView_4->model()->data(ui->tableView_4->model()->index(ui->tableView_4->selectionModel()->currentIndex().row(),5)).toString());
+     ui->tel_modif_en->setText(ui->tableView_4->model()->data(ui->tableView_4->model()->index(ui->tableView_4->selectionModel()->currentIndex().row(),4)).toString());
+
+    ui->id_modif_en->setText(ui->tableView_4->model()->data(ui->tableView_4->model()->index(ui->tableView_4->selectionModel()->currentIndex().row(),0)).toString());
+    ui->stackedWidget_2->setCurrentIndex(2);
+
+}
+
+void sport_entraineur::on_modifier_2_clicked()
+{
+    entraineur s;
+ajout->play();
+    if(s.modifier(ui))
+    {
+        QMessageBox ::information(this,"","entraineur modifié")  ;
+        ui->stackedWidget_2->setCurrentIndex(1);
+        s.afficher(ui);
+    }
+    else
+    { QMessageBox ::critical(this,"","erreur de modification") ;}
+}
+
+void sport_entraineur::on_ajouter_clicked()
+{
+    ajout->play();
+        QRegExp regex1 ("[a-z]$");
+        QRegExp regex2 ("^[a-z]");
+        QRegExp regex3("\\b[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}\\b");
+        QString msg_nom ="nom invalide";
+        QString msg_prenom ="prenom invalide";
+         QString msg_email ="email invalide";
+
+        QString msg_vide="";
+
+        int id=ui->id_ent->text().toInt();
+        QString nom=ui->nom_ent->text();
+        QString prenom=ui->prenom_ent->text();
+        QString DAT=ui->date_ent->text();
+        int tel=ui->tel_ent->text().toInt();
+         QString email=ui->email_ent->text();
+
+
+
+        entraineur F(id,nom,prenom,DAT,email,tel);
+        if ((!nom.contains(regex1))||(!nom.contains(regex2)))
+        {
+            ui->nom_ent->setStyleSheet("QLineEdit { color: red;}");
+            ui->text_nom->setText(msg_nom);
+            ui->text_nom->setStyleSheet("QLabel { background-color : transparent; color : red; }");
+            F.setValide();
+        }
+        else { ui->nom_ent->setStyleSheet("QLineEdit { color: black;}");
+            ui->text_nom->setText(msg_vide);
+
+        }
+
+        if ((!prenom.contains(regex1))||(!prenom.contains(regex2)))
+        {
+            ui->prenom_ent->setStyleSheet("QLineEdit { color: red;}");
+            ui->text_prenom->setText(msg_prenom);
+            ui->text_prenom->setStyleSheet("QLabel { background-color : transparent; color : red; }");
+            F.setValide();
+        }
+        else { ui->prenom_ent->setStyleSheet("QLineEdit { color: black;}");
+            ui->text_prenom->setText(msg_vide);
+
+        }
+
+        if (!email.contains(regex3))
+        {
+            ui->email_ent->setStyleSheet("QLineEdit { color: red;}");
+            ui->text_email->setText(msg_email);
+            ui->text_email->setStyleSheet("QLabel { background-color : transparent; color : red; }");
+            F.setValide();
+        }
+        else { ui->email_ent->setStyleSheet("QLineEdit { color: black;}");
+            ui->text_email->setText(msg_vide);
+
+        }
+
+
+        if (F.getValide()==0)
+        {
+        if(F.ajouter()){
+            ui->nom_ent->setStyleSheet("QLineEdit { color: green;}");
+            ui->prenom_ent->setStyleSheet("QLineEdit { color: green;}");
+
+              ui->email_ent->setStyleSheet("QLineEdit { color: green;}");
+            QMessageBox::information(nullptr, QObject::tr("OK"),
+                        QObject::tr("ajout de entraineur succesful.\n"
+                                    "Click Cancel to exit."), QMessageBox::Cancel);
+              //ui->tableView->setModel(F.afficher());
+        }
+        else{
+
+            QMessageBox::critical(nullptr, QObject::tr("NOT OK"),
+                        QObject::tr("ajout failed.\n"
+                                    "Click Cancel to exit."), QMessageBox::Cancel);
+        }
+
+
+
+    }
+}
+
+void sport_entraineur::on_traiter_clicked()
+{
+    son->play();
+    entraineur s;
+    s.afficher(ui);
+    ui->stackedWidget_2->setCurrentIndex(1);
+
+}
+
+void sport_entraineur::on_entraineur_ajout_clicked()
+{
+    son->play();
+    entraineur s;
+    ui->stackedWidget_2->setCurrentIndex(0);
+    s.ajouter();
+}
+
+void sport_entraineur::on_Annuler_2_clicked()
+{
+    son->play();
+    ui->stackedWidget_2->setCurrentIndex(1);
+}
+
+
+void sport_entraineur::on_image_clicked()
+{
+son->play();
+   sport s;
+   s.image();
+
+}
+
+void sport_entraineur::on_mailing_clicked()
+{
+    QDialog *d=new Dialog(this);
+    d->show();
+
+    d->exec();
+}
