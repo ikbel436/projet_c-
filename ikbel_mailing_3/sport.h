0b9662f85c33a1978ebe6da034ecd4413f9ef47b
ikbel436 <ikbel.benmansour@esprit.tn>
2021-04-26 14:47:28 +0100

    ikbel val ajoutée

---
+++ ikbel_mailing_3/sport.h
@@ -0,0 +1,52 @@
+#ifndef SPORT_H
+#define SPORT_H
+#include <QString>
+#include <QSqlQuery>
+#include <QSqlQueryModel>
+#include <QtCore/qglobal.h>
+#include <QDate>
+//#include "sport_entraineur.h"
+
+class sport
+{
+public:
+    sport();
+    sport(int,QString,QString,int);
+
+    int getID();
+    QString getNom();
+    QString getdat();
+    int getnb();
+
+
+    void setId(int);
+    void setNom(QString);
+    void setdat(QString);
+    void setnb(int);
+    bool ajouter();
+
+/*void afficher_sport(Ui:: sport_entraineur *ui);
+bool supprimer_sport(Ui:: sport_entraineur *ui);
+void Tri_DAT(Ui:: sport_entraineur *ui) ;
+ void Tri_NOM(Ui:: sport_entraineur *ui);
+ void Tri_NB(Ui:: sport_entraineur *ui);
+ bool Supprimer_sport(Ui:: sport_entraineur *ui);
+ bool modifier_sport (Ui:: sport_entraineur *ui);
+void Recherche_sport(Ui:: sport_entraineur *ui);
+void image();*/
+    int recupererID(QString nom);
+    QList<sport> afficherRole();
+    sport afficherR(int id);
+
+
+
+void on_Valider_clicked();
+ int getValide();
+ void setValide();
+private:
+    int id,nb ;
+    QString nom,dat;
+    int valide=0;
+};
+
+#endif // SPORT_H
