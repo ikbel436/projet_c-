5f4bdcd67e7d7b3731c1bd1adccf32533fcb4cd0
MedAzizBenzarti <mohamedaziz.benzarit@esprit.tn>
2021-04-28 00:16:02 +0100

    final valeur ajoute playson+sport_metier

---
+++ aziz_final_qt/email_entraineur.h
@@ -0,0 +1,33 @@
+#ifndef EMAIL_ENTRAINEUR_H
+#define EMAIL_ENTRAINEUR_H
+
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+class Dialog;
+}
+
+class Dialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Dialog(QWidget *parent = nullptr);
+    explicit Dialog(QString,QString,QString,QWidget *parent = nullptr);
+    ~Dialog();
+    void set_tmpemail(QString e){tmpemail=e;}
+
+private slots:
+    void on_pushButton_2_clicked();
+
+private:
+    Ui::Dialog *ui;
+    QString tmpemail;
+};
+
+#endif // DIALOG_H
+
+#endif // EMAIL_ENTRAINEUR_H
