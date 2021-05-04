86f44f9d6692ffc606ed456c0fe0c0df8bf5a7b0
samarbouzezi <samar.bouzezi@esprit.tn>
2021-05-05 00:10:11 +0100

    +camera +total

---
+++ camera ok/build-samar-Desktop_Qt_5_12_10_MinGW_32_bit-Debug/debug/moc_empfou.cpp
@@ -0,0 +1,244 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'empfou.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.10)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../Samar -/empfou.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'empfou.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.10. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_empfou_t {
+    QByteArrayData data[32];
+    char stringdata0[697];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_empfou_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_empfou_t qt_meta_stringdata_empfou = {
+    {
+QT_MOC_LITERAL(0, 0, 6), // "empfou"
+QT_MOC_LITERAL(1, 7, 24), // "on_supprimer_emp_clicked"
+QT_MOC_LITERAL(2, 32, 0), // ""
+QT_MOC_LITERAL(3, 33, 35), // "on_comboBox_emp_currentIndexC..."
+QT_MOC_LITERAL(4, 69, 5), // "index"
+QT_MOC_LITERAL(5, 75, 30), // "on_tableView_emp_doubleClicked"
+QT_MOC_LITERAL(6, 106, 11), // "QModelIndex"
+QT_MOC_LITERAL(7, 118, 23), // "on_modifier_emp_clicked"
+QT_MOC_LITERAL(8, 142, 20), // "on_excel_emp_clicked"
+QT_MOC_LITERAL(9, 163, 22), // "on_Valider_emp_clicked"
+QT_MOC_LITERAL(10, 186, 22), // "on_Annuler_emp_clicked"
+QT_MOC_LITERAL(11, 209, 14), // "on_emp_clicked"
+QT_MOC_LITERAL(12, 224, 25), // "on_supprimer_four_clicked"
+QT_MOC_LITERAL(13, 250, 36), // "on_comboBox_four_currentIndex..."
+QT_MOC_LITERAL(14, 287, 22), // "on_search_four_clicked"
+QT_MOC_LITERAL(15, 310, 21), // "on_four_ajout_clicked"
+QT_MOC_LITERAL(16, 332, 24), // "on_recherche_textChanged"
+QT_MOC_LITERAL(17, 357, 4), // "arg1"
+QT_MOC_LITERAL(18, 362, 19), // "on_buttonon_clicked"
+QT_MOC_LITERAL(19, 382, 20), // "on_buttonoff_clicked"
+QT_MOC_LITERAL(20, 403, 12), // "Timefunction"
+QT_MOC_LITERAL(21, 416, 21), // "on_actualiser_clicked"
+QT_MOC_LITERAL(22, 438, 25), // "on_push_photo_emp_clicked"
+QT_MOC_LITERAL(23, 464, 30), // "on_pushButton_modifier_clicked"
+QT_MOC_LITERAL(24, 495, 26), // "on_pushButton_lire_clicked"
+QT_MOC_LITERAL(25, 522, 23), // "on_ajouter_four_clicked"
+QT_MOC_LITERAL(26, 546, 24), // "on_modifier_four_clicked"
+QT_MOC_LITERAL(27, 571, 29), // "on_Annuler_four_modif_clicked"
+QT_MOC_LITERAL(28, 601, 31), // "on_tableView_four_doubleClicked"
+QT_MOC_LITERAL(29, 633, 22), // "on_ajouter_emp_clicked"
+QT_MOC_LITERAL(30, 656, 16), // "on_four2_clicked"
+QT_MOC_LITERAL(31, 673, 23) // "on_pushButton_4_clicked"
+
+    },
+    "empfou\0on_supprimer_emp_clicked\0\0"
+    "on_comboBox_emp_currentIndexChanged\0"
+    "index\0on_tableView_emp_doubleClicked\0"
+    "QModelIndex\0on_modifier_emp_clicked\0"
+    "on_excel_emp_clicked\0on_Valider_emp_clicked\0"
+    "on_Annuler_emp_clicked\0on_emp_clicked\0"
+    "on_supprimer_four_clicked\0"
+    "on_comboBox_four_currentIndexChanged\0"
+    "on_search_four_clicked\0on_four_ajout_clicked\0"
+    "on_recherche_textChanged\0arg1\0"
+    "on_buttonon_clicked\0on_buttonoff_clicked\0"
+    "Timefunction\0on_actualiser_clicked\0"
+    "on_push_photo_emp_clicked\0"
+    "on_pushButton_modifier_clicked\0"
+    "on_pushButton_lire_clicked\0"
+    "on_ajouter_four_clicked\0"
+    "on_modifier_four_clicked\0"
+    "on_Annuler_four_modif_clicked\0"
+    "on_tableView_four_doubleClicked\0"
+    "on_ajouter_emp_clicked\0on_four2_clicked\0"
+    "on_pushButton_4_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_empfou[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      27,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  149,    2, 0x08 /* Private */,
+       3,    1,  150,    2, 0x08 /* Private */,
+       5,    1,  153,    2, 0x08 /* Private */,
+       7,    0,  156,    2, 0x08 /* Private */,
+       8,    0,  157,    2, 0x08 /* Private */,
+       9,    0,  158,    2, 0x08 /* Private */,
+      10,    0,  159,    2, 0x08 /* Private */,
+      11,    0,  160,    2, 0x08 /* Private */,
+      12,    0,  161,    2, 0x08 /* Private */,
+      13,    1,  162,    2, 0x08 /* Private */,
+      14,    0,  165,    2, 0x08 /* Private */,
+      15,    0,  166,    2, 0x08 /* Private */,
+      16,    1,  167,    2, 0x08 /* Private */,
+      18,    0,  170,    2, 0x08 /* Private */,
+      19,    0,  171,    2, 0x08 /* Private */,
+      20,    0,  172,    2, 0x08 /* Private */,
+      21,    0,  173,    2, 0x08 /* Private */,
+      22,    0,  174,    2, 0x08 /* Private */,
+      23,    0,  175,    2, 0x08 /* Private */,
+      24,    0,  176,    2, 0x08 /* Private */,
+      25,    0,  177,    2, 0x08 /* Private */,
+      26,    0,  178,    2, 0x08 /* Private */,
+      27,    0,  179,    2, 0x08 /* Private */,
+      28,    1,  180,    2, 0x08 /* Private */,
+      29,    0,  183,    2, 0x08 /* Private */,
+      30,    0,  184,    2, 0x08 /* Private */,
+      31,    0,  185,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void, 0x80000000 | 6,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,   17,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 6,    4,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void empfou::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<empfou *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_supprimer_emp_clicked(); break;
+        case 1: _t->on_comboBox_emp_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->on_tableView_emp_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 3: _t->on_modifier_emp_clicked(); break;
+        case 4: _t->on_excel_emp_clicked(); break;
+        case 5: _t->on_Valider_emp_clicked(); break;
+        case 6: _t->on_Annuler_emp_clicked(); break;
+        case 7: _t->on_emp_clicked(); break;
+        case 8: _t->on_supprimer_four_clicked(); break;
+        case 9: _t->on_comboBox_four_currentIndexChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->on_search_four_clicked(); break;
+        case 11: _t->on_four_ajout_clicked(); break;
+        case 12: _t->on_recherche_textChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 13: _t->on_buttonon_clicked(); break;
+        case 14: _t->on_buttonoff_clicked(); break;
+        case 15: _t->Timefunction(); break;
+        case 16: _t->on_actualiser_clicked(); break;
+        case 17: _t->on_push_photo_emp_clicked(); break;
+        case 18: _t->on_pushButton_modifier_clicked(); break;
+        case 19: _t->on_pushButton_lire_clicked(); break;
+        case 20: _t->on_ajouter_four_clicked(); break;
+        case 21: _t->on_modifier_four_clicked(); break;
+        case 22: _t->on_Annuler_four_modif_clicked(); break;
+        case 23: _t->on_tableView_four_doubleClicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        case 24: _t->on_ajouter_emp_clicked(); break;
+        case 25: _t->on_four2_clicked(); break;
+        case 26: _t->on_pushButton_4_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject empfou::staticMetaObject = { {
+    &QDialog::staticMetaObject,
+    qt_meta_stringdata_empfou.data,
+    qt_meta_data_empfou,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *empfou::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *empfou::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_empfou.stringdata0))
+        return static_cast<void*>(this);
+    return QDialog::qt_metacast(_clname);
+}
+
+int empfou::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QDialog::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 27)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 27;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 27)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 27;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
